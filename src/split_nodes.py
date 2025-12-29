cc22f1ba5b5d9e7810fb23001623832cea98f402
Joran-Vini <joranvinicius@gmail.com>
2025-12-29 20:05:09 -0300

    finished split_nodes, making the split_images and links

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract_markdown import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -22,3 +23,50 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(part, text_type))
         
     return new_nodes    
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        parts = extract_markdown_images(node.text)
+
+        if len(parts) == 0:
+            new_nodes.append(node)
+            continue
+        
+        for alt, url in parts:
+            sections = original_text.split(f"![{alt}]({url})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            original_text = sections[1]
+        if original_text:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        parts = extract_markdown_links(node.text)
+
+        if len(parts) == 0:
+            new_nodes.append(node)
+            continue
+        
+        for alt, url in parts:
+            sections = original_text.split(f"[{alt}]({url})", 1)
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.LINK, url))
+            original_text = sections[1]
+        if original_text:
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
